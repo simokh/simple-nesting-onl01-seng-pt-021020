2baedfb3e12ad81751800dbf32920db76e9d3aa1
Simo Khouili <m.simokhouili@gmail.com>
2020-02-23 16:42:13 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -129,7 +129,7 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-dennis_new_info = "Assembly"
+# dennis_new_info = "Assembly"
 programmer_hash[:dennis_ritchie][:languages] = "Assembly"
 programmer_hash
 end
