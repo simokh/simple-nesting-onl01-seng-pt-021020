2791a403c2a880e46a45ad9dbc899fafe9e22df2
Simo Khouili <m.simokhouili@gmail.com>
2020-02-23 16:23:39 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -16,7 +16,12 @@ def hopper
         }
      }
 
-
+		 programmer_hash = 
+		 	{
+		 			:grace_hopper => {
+		 				:known_for => "COBOL",
+		 				:languages => ["COBOL", "FORTRAN"]
+		 			},
 end
 
 def alan_kay_is_known_for
